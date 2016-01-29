f23e6212f49d4b60daed5c5859de9c6720ca40e2
johannarvaez97 <jenarvaez80@misena.edu.co>
2016-01-29 14:42:27 -0500

    Merge Gnome into Drivers
    Conflicts:
    	Ejercicio6/src/ejercicio6/Ejercicio6.java

--- Ejercicio6/src/ejercicio6/Ejercicio6.java
+++ Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -15,9 +15,10 @@ public class Ejercicio6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("linux version 1");    
-        System.out.println("GNOME");}
-        System.out.println("linux version 1");  
-        System.out.println("drivers ");}
-    
+        System.out.println("linux version 1");
+        System.out.println("GNOME");
+        System.out.println("linux version 1");
+        System.out.println("drivers ");
+    }
+
 }
